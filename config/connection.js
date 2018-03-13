35fd46df7dd42158d9118226935b51fe2f15e837
William Robertson <williamp.robertson@gmail.com>
2018-03-13 00:58:21 -0700

    heroku jaws db connection testing

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  connection = mysql.createConnection({
+    root: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
